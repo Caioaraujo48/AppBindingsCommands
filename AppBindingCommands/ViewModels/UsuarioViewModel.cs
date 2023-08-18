aedc64594db6523d2488ceeae5ae661eed6a42be
Etec Prof. Horácio Augusto da Silveira <caioquir@gmail.com>
2023-08-17 21:27:39 -0300

    Incompleto

---
+++ AppBindingCommands/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponenteModel;
+using System.ComponenteModel.Implement.Interface;
+
+
+public class UsuarioViewModel : INotifyPropertyChanged
+{
+	public event PropertyChangedEvenHandler PropertyChanged;
+	
+	void OnPropertyChanged(string propertyName)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+	private string name = string.Empty;
+
+	public string Name
+	{
+		get => name;
+		set
+		{
+			if (name == null)
+				return;
+
+			name = value;
+			OnPropertyChanged(nameof(name));
+			OnPropertyChanged(nameof(DisplayName));
+		}
+	}
+}
